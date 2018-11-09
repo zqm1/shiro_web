60f77266fa9917c934eb164e60d78fc4470c9696
zqm1 <442140118@qq.com>
2018-11-09 18:21:42 +0800

    新增

---
+++ src/main/java/com/zhu/shiro/spring/Constants.java
@@ -0,0 +1,14 @@
+package com.zhu.shiro.spring;
+
+/**
+ * <p>
+ * User: Zhang Kaitao
+ * <p>
+ * Date: 14-2-26
+ * <p>
+ * Version: 1.0
+ */
+public class Constants {
+	public static final String PARAM_DIGEST = "digest";
+	public static final String PARAM_USERNAME = "username";
+}
